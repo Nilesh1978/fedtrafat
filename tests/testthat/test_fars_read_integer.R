f94f509edb2a7f9df8741e15c779d8f908c14e91
Nilesh Dighe <nilesh.dighe@monsanto.com>
2017-07-26 12:04:43 -0500

    added libraries to the test folder

--- tests/testthat/test_fars_read_integer.R
+++ tests/testthat/test_fars_read_integer.R
@@ -4,4 +4,8 @@ make_file_name_test_year <- function(year) {
   #sprintf("accident_%d.csv.bz2", year)
 }
 
+library(testthat)
+library(fedtrafat)
 expect_that(class(make_file_name_test_year(2003)), is_equivalent_to("integer"))
+
+
